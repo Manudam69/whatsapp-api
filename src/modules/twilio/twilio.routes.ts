fc8ad5e87849e8c53e007ed4fb24a39f08c7a86d
Manuel Alonso <manueldavid@live.com.mx>
2023-10-10 13:42:26 -0600

    Update twilio.routes.ts

--- src/modules/twilio/twilio.routes.ts
+++ src/modules/twilio/twilio.routes.ts
@@ -6,14 +6,15 @@ import {
   send_to_approval_validator,
   use_template_validator,
 } from './twilio.validator'
+import { validateBody } from '@middlewares/validator'
 
 const router = Router()
 
-router.post('/create-media-template', [...create_media_template_validator], Controller.create_media_template)
+router.post('/create-media-template', [...create_media_template_validator, validateBody], Controller.create_media_template)
 router.get('/template/:content_sid', Controller.get_template)
-router.put('/send-to-approval', [...send_to_approval_validator], Controller.send_template_to_approval)
+router.put('/send-to-approval', [...send_to_approval_validator, validateBody], Controller.send_template_to_approval)
 router.get('/templates', Controller.get_templates)
-router.put('/use-template', [...use_template_validator], Controller.use_template)
-router.delete('/delete-template', [...delete_template_validator], Controller.delete_template)
+router.put('/use-template', [...use_template_validator, validateBody], Controller.use_template)
+router.delete('/delete-template', [...delete_template_validator, validateBody], Controller.delete_template)
 
 export default router
