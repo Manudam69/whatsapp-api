98bb62e5f9d3c2eaa0e13327fdabbea52a8b73c7
Manuel Alonso <malonso@gobdigital.com>
2023-10-11 12:51:26 -0600

    Text and List templates

--- build/modules/twilio/twilio.controller.js
+++ build/modules/twilio/twilio.controller.js
@@ -9,8 +9,21 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.delete_template = exports.use_template = exports.get_templates = exports.send_template_to_approval = exports.get_template = exports.create_media_template = void 0;
+exports.delete_template = exports.use_template = exports.get_templates = exports.send_template_to_approval = exports.get_template = exports.create_list_picker_template = exports.create_media_template = exports.create_text_template = void 0;
 const services_1 = require("./services");
+function create_text_template(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const service = new services_1.TwilioService();
+        try {
+            const template = yield service.create_text_template(req.body);
+            res.json(template);
+        }
+        catch (error) {
+            next(error);
+        }
+    });
+}
+exports.create_text_template = create_text_template;
 function create_media_template(req, res, next) {
     return __awaiter(this, void 0, void 0, function* () {
         const service = new services_1.TwilioService();
@@ -24,6 +37,19 @@ function create_media_template(req, res, next) {
     });
 }
 exports.create_media_template = create_media_template;
+function create_list_picker_template(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const service = new services_1.TwilioService();
+        try {
+            const template = yield service.create_list_picker_template(req.body);
+            res.json(template);
+        }
+        catch (error) {
+            next(error);
+        }
+    });
+}
+exports.create_list_picker_template = create_list_picker_template;
 function get_template(req, res, next) {
     return __awaiter(this, void 0, void 0, function* () {
         const service = new services_1.TwilioService();
@@ -81,8 +107,8 @@ function delete_template(req, res, next) {
     return __awaiter(this, void 0, void 0, function* () {
         const service = new services_1.TwilioService();
         try {
-            const { content_sid } = req.params;
-            const template = yield service.delete_template(content_sid);
+            const { content_sid } = req.query;
+            const template = yield service.delete_template(content_sid.toString());
             res.json(template);
         }
         catch (error) {
--- build/modules/twilio/twilio.validator.js
+++ build/modules/twilio/twilio.validator.js
@@ -1,18 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.delete_template_validator = exports.use_template_validator = exports.send_to_approval_validator = exports.create_media_template_validator = void 0;
+exports.delete_template_validator = exports.use_template_validator = exports.send_to_approval_validator = exports.create_list_picker_validator = exports.create_media_template_validator = exports.create_text_template_validator = void 0;
 const express_validator_1 = require("express-validator");
+exports.create_text_template_validator = [(0, express_validator_1.check)('name').notEmpty().withMessage('{name} was expected')];
 exports.create_media_template_validator = [
     (0, express_validator_1.check)('name').notEmpty().withMessage('{name} was expected').isString().withMessage('string type was expected'),
     (0, express_validator_1.check)('message').notEmpty().withMessage('{message} was expected').isString().withMessage('string type was expected'),
     (0, express_validator_1.check)('media').notEmpty().withMessage('{media} was expected').isString().withMessage('string type was expected'),
+];
+exports.create_list_picker_validator = [
+    (0, express_validator_1.check)('name').notEmpty().withMessage('{name} was expected').isString().withMessage('string type was expected'),
+    (0, express_validator_1.check)('list_message')
+        .notEmpty()
+        .withMessage('{list_message} was expected')
+        .isString()
+        .withMessage('string type was expected'),
+    (0, express_validator_1.check)('label').notEmpty().withMessage('{label} was expected').isString().withMessage('string type was expected'),
+    (0, express_validator_1.check)('items').isArray({ min: 1 }).withMessage('Array {items} was expected'),
 ];
 exports.send_to_approval_validator = [
     (0, express_validator_1.check)('name').notEmpty().withMessage('{name} was expected').isString().withMessage('string type was expected'),
     (0, express_validator_1.check)('category')
         .notEmpty()
         .withMessage('{category} was expected')
-        .isIn(['Authentication', 'Utility', 'Marketing'])
+        .isIn(['authentication', 'utility', 'marketing'])
         .withMessage('Only [Authentication, Utility, Marketing] types are accepted'),
     (0, express_validator_1.check)('content_sid')
         .notEmpty()
